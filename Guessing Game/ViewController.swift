ceb276b8a665bbc1b812925c131df30868baf33f
ashleydonohoe <ashleyrdonohoe@gmail.com>
2015-08-03 01:53:09 -0400

    first commit

---
+++ Guessing Game/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Guessing Game
+//
+//  Created by Gabriele on 8/3/15.
+//  Copyright © 2015 Ashley D Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var userGuess: UITextField!
+    
+    @IBOutlet var userResult: UILabel!
+
+    @IBAction func guessButton(sender: AnyObject) {
+        
+        let randomNumber = String(arc4random_uniform(6))
+        
+        if randomNumber == userGuess.text {
+            userResult.text = "You're right"
+        } else {
+            userResult.text = "Wrong! It was a \(randomNumber)"
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
